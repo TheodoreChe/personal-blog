ac8029d86c70bdbfa6902f52f1741a87abe96430
theodoreche <2226390@gmail.com>
2020-10-17 22:55:59 +0300

    feat: edit home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,32 @@
 import React from 'react';
-import { Layout } from '../components/layout';
-import styles from '../styles/Home.module.css';
+import { GetStaticProps } from 'next';
+import { getPost, Post } from 'lib/markdown';
+import { Layout } from 'components/layout';
+import { PostList } from 'components/postList';
 
 type Props = {
-    title: string;
+    postList: Array<Post>;
 };
 
 const Home: React.FC<Props> = (props: Props) => {
     return (
-        <Layout title="Home">
-            <main className={styles.main}>
-                <h1 className={styles.title}>{props.title}</h1>
-
-                <p className={styles.description}>
-                    Get started by editing <code className={styles.code}>pages/index.js</code>
-                </p>
-
-                <div className={styles.grid}>
-                    <a href="https://nextjs.org/docs" className={styles.card}>
-                        <h3>Documentation &rarr;</h3>
-                        <p>Find in-depth information about Next.js features and API.</p>
-                    </a>
-
-                    <a href="https://nextjs.org/learn" className={styles.card}>
-                        <h3>Learn &rarr;</h3>
-                        <p>Learn about Next.js in an interactive course with quizzes!</p>
-                    </a>
-
-                    <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
-                        <h3>Examples &rarr;</h3>
-                        <p>Discover and deploy boilerplate example Next.js projects.</p>
-                    </a>
-
-                    <a
-                        href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className={styles.card}
-                    >
-                        <h3>Deploy &rarr;</h3>
-                        <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-                    </a>
-                </div>
-            </main>
-
-            <footer className={styles.footer}>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-                </a>
-            </footer>
+        <Layout>
+            <PostList postList={props.postList} />
         </Layout>
     );
 };
 
-const getStaticProps = async () => {
-    const title = 'Hello';
+const getStaticProps: GetStaticProps = async () => {
+    const postNameList = require
+        .context('markdown/posts')
+        .keys()
+        .map((k: string) => k.match(/\.\/(.*)\./)?.[1]);
+
+    const postList = await Promise.all(postNameList.map(getPost));
+
     return {
         props: {
-            title,
+            postList,
         },
     };
 };
