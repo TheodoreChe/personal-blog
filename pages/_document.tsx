506e4ce1abb76f82525174335c20de9c7c8c2e1d
theodoreche <2226390@gmail.com>
2020-10-17 22:55:28 +0300

    feat: add google fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-    render() {
+    render(): ReactElement {
         return (
             <Html lang="en">
-                <Head />
-                <body>
+                <Head>
+                    <title>Theodore Che</title>
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap"
+                    />
+                </Head>
+                <body className="dark">
                     <Main />
                     <NextScript />
                 </body>
