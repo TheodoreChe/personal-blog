60f9502be664ba0550c2d09bd2a2265c65cd17e4
theodoreche <2226390@gmail.com>
2020-12-12 01:22:28 +0300

    feat: edit main layout component

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { TYPE_OF_THEME } from 'constants/style';
+import { AppContext } from '../AppProvider/AppProvider';
+import { Breadcrumb } from '../Breadcrumb';
+import { Header } from './components';
+import styles from './Layout.module.css';
+
+type Props = {
+    children: React.ReactNode;
+    pageName?: string;
+    withHeader?: boolean;
+};
+
+export const Layout: React.FC<Props> = (props: Props) => {
+    const { typeOfTheme } = React.useContext(AppContext);
+    const classNameList = [styles.main, typeOfTheme === TYPE_OF_THEME.DARK ? styles.dark : styles.light]
+        .filter(Boolean)
+        .join(' ');
+    return (
+        <main className={classNameList}>
+            {props.pageName && <Breadcrumb pageName={props.pageName} />}
+            {props.withHeader && <Header />}
+            <div className={styles.wrapper}>{props.children}</div>
+        </main>
+    );
+};
--- components/Layout/components/Header/Header.tsx
+++ components/Layout/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Typography } from '../typography';
-import { Image } from '../image';
+import { DarkModeToggle } from 'components/DarkModeToggle';
+import { Typography } from 'components/Typography';
+import { Image } from 'components/Image';
 import styles from './Header.module.css';
 
 const HEADER_TITLE = 'Theodore Chernin';
@@ -10,7 +11,7 @@ export const Header: React.FC = () => (
     <header className={styles.header}>
         <div className={styles.wrapper}>
             <div className={styles.avatar}>
-                <Image src="./assets/theodore.chernin.png" alt="Theodore Chernin Avatar" />
+                <Image src="/assets/theodore.chernin.png" alt="Theodore Chernin Avatar" />
             </div>
             <div>
                 <Typography as="h1" color="gradient" size="small">
@@ -19,6 +20,9 @@ export const Header: React.FC = () => (
                 <Typography as="span" color="gray">
                     {HEADER_DESC}
                 </Typography>
+            </div>
+            <div className={styles.toggle}>
+                <DarkModeToggle />
             </div>
         </div>
     </header>
